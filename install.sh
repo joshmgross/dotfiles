e7cd632a0b085c3289f1729f1744cddb69aa09c7
Josh Gross <joshmgross@github.com>
2021-06-02 14:31:32 +0000

    Add logging to install script

--- install.sh
+++ install.sh
@@ -2,6 +2,20 @@
 #
 # bootstrap installs things.
 
+
+SCRIPT_NAME=${1:-"install.sh"}
+# Send all our -x output to this file for later debugging
+LOG_DIR="$HOME/install.sh.logs"
+mkdir -p "${LOG_DIR}"
+exec 1>"${LOG_DIR}/stdout"
+exec 2>"${LOG_DIR}/stderr"
+exec 19>"${LOG_DIR}/trace"
+BASH_XTRACEFD=19
+
+set -x
+
+echo "${SCRIPT_NAME} start: $(date)"
+
 DOTFILES_ROOT=$(pwd -P)
 
 set -e
