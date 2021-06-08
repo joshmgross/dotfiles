ff8886dbe6a7e40e1be9000d490ec1c50e983988
Josh Gross <joshmgross@github.com>
2021-06-08 00:42:04 +0000

    📰 Send stdout to file

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ SCRIPT_NAME=${1:-"install.sh"}
 # Send all our -x output to this file for later debugging
 LOG_DIR="$HOME/install.sh.logs"
 mkdir -p "${LOG_DIR}"
-# exec 1>"${LOG_DIR}/stdout"
+exec 1>"${LOG_DIR}/stdout"
 exec 2>"${LOG_DIR}/stderr"
 exec 19>"${LOG_DIR}/trace"
 BASH_XTRACEFD=19
